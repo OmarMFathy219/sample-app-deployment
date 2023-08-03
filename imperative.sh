48a2a4e3c9cb271f7449b815fa5e5e3ca5a2b4d4
Omar M. Fathy <OmarMFathy219@gmail.com>
2023-08-03 05:43:09 +0300

    Update imperative.sh

--- imperative.sh
+++ imperative.sh
@@ -1,9 +1,13 @@
+# create namespace
+kubectl create namespace sandbox
+
 # Create deployment with name simple-deployment
-kubectl create deployment simple-deployment3 --image=docker.io/kostiscodefresh/gitops-simple-app:v1.0 
+kubectl create deployment simple-deployment --image=docker.io/kostiscodefresh/gitops-simple-app:v1.0 --namespace=sandbox
 
 # Scale deployment to 1 replica
-kubectl scale deployment simple-deployment3 --replicas=3
-
+kubectl scale deployment simple-deployment --replicas=3 --namespace=sandbox
 # Expose deployment as a service
-kubectl expose deployment simple-deployment3 --name=simple-service3 --type=NodePort --port=3100 --target-port=8080
+kubectl expose deployment simple-deployment --name=simple-service --type=NodePort --port=3100 --target-port=8080 --namespace=sandbox
 
+# Port-forward service to localhost
+kubectl port-forward service/simple-service 3100:3100 --namespace=sandbox &
